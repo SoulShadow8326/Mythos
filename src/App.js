e66891a154dd61fa8a3b32dc4c496c6af9d75d56
SoulShadow8326 <soulshadow8326@gmail.com>
2025-07-30 08:46:32 +0530

    added story route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
+import StoryPage from './pages/StoryBuilder';
 import './styles/global.css';
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/story" element={<StoryPage />} />
           </Routes>
         </main>
       </div>
